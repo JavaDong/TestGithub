a0084b730d89a11b43d7c959210b949746c9486d
JavaDong <407607953@qq.com>
2017-05-31 15:12:36 +0800

    测试

---
+++ src/main/java/com/zsrt/java/kaoshi/App.java
@@ -0,0 +1,17 @@
+package com.zsrt.java.kaoshi;
+
+import org.dom4j.io.SAXReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        SAXReader xx;
+    }
+}
